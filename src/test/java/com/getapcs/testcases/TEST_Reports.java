62fe70740cf2885e1a4f521a47e149b7cd52d23a
W2191 <W2191@BLLCHETHANV.WYZMINDZ.COM>
2023-12-27 16:26:16 +0530

    commit

--- src/test/java/com/getapcs/testcases/TEST_Reports.java
+++ src/test/java/com/getapcs/testcases/TEST_Reports.java
@@ -1,5 +1,38 @@
 package com.getapcs.testcases;
 
-public class TEST_Reports {
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.getapcs.Keus.Reports.SalesOrder_Reports;
+import com.getapcs.base.Testbase1;
+import com.getapcs.pages.HomePage;
+import com.getapcs.pages.LoginPage;
+
+public class TEST_Reports extends Testbase1 {
+
+	LoginPage loginPage;
+	HomePage homePage;
+	SalesOrder_Reports SalesOrder_Reports;
+
+	public TEST_Reports() {
+		super();
+	}
+
+	@BeforeTest
+	public void setUp() throws Throwable {
+		initialization();
+		loginPage = new LoginPage();
+		homePage = loginPage.login("admin@mail.com", "user@123");
+
+		SalesOrder_Reports = new SalesOrder_Reports();
+
+	}
+
+	@Test(priority = 1)
+	public void verifyPrice_ListCreate() throws Exception {
+		homePage.clickOnSalesOrderReport();
+		SalesOrder_Reports.SalesOrder_ReportsPage();
+		driver.navigate().to("https://demo_keus.getapcs.com/dashboard");
+	}
 
 }
