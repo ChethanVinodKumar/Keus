11a58852486057c9f1b1642b7430630e2a40093d
Gopal Reddy <gpalredy05@gmail.com>
2023-12-27 15:28:05 +0530

    commit 27

--- src/main/java/com/getapcs/Keus/Reports/ItemMaster_Reports.java
+++ src/main/java/com/getapcs/Keus/Reports/ItemMaster_Reports.java
@@ -1,5 +1,14 @@
 package com.getapcs.Keus.Reports;
 
-public class ItemMaster_Reports {
+import org.openqa.selenium.support.PageFactory;
+
+import com.getapcs.base.Testbase1;
+
+public class ItemMaster_Reports extends Testbase1 {
+
+	public ItemMaster_Reports() {
+
+		PageFactory.initElements(driver, this);
+	}
 
 }
